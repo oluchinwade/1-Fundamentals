81b796d4c9f57a8866ff66235a065aa7e3edec29
Oluchukwu Nwade <oluchinwade@gmail.com>
2023-10-15 22:04:52 +0100

    Data Structures

--- list.py
+++ list.py
@@ -1,9 +1,33 @@
-new_list = ['food', 'man','apple','fashion']
+'''new_list = ['food', 'man','apple','fashion']
 
 list2 = list(new_list)
 
 new_list.append('bootcamp')
 
-print(list2, new_list)
+print(list2, new_list)'''
+
+
+import random
+
+
+diamonds = ["AD", "2D", "3D", "4D", "5D", "6D",
+            "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+hand = []
+
+while diamonds:
+    user_input = input("Press 'Enter' to pick a card or Press 'Q' then enter to quit:  ").lower()
+    if user_input == "q":
+        print("Sad to see you go. Bye!")
+        break
+    else:
+        new_list = random.choice(diamonds)
+        hand.append(new_list)
+        diamonds.remove(new_list)
+        print(f"Your hand: {hand}")
+        print(f"Remaining cards: {diamonds}")    
+    if not diamonds:
+        print("There are no more cards to pick \nThank you playing, Bye! ")
+        break
+
 
 
---
+++ workshop3/app.py
@@ -0,0 +1,7 @@
+from donation_pkg import homepage
+
+database = {"admin":"password123"}
+donation = []
+authorized_user = ""
+homepage.show_homepage()
+
