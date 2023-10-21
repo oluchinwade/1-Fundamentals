45c000d0dfa1e305c390f98f71277a444973b766
Oluchukwu Nwade <oluchinwade@gmail.com>
2023-10-21 14:23:20 +0100

    stack and queue

---
+++ WEEK4/queue1.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.head =None
+        self.tail =None
+        self.num_nodes = 0
+
+    #creating a method to return the size of the queue
+    def size(self):
+        return self.num_nodes
+    # creating a function to add to the end of the queue
+    def enqueue(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = self.tail = new_node
+
+        
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.num_nodes +=1
+    
+
+    #create a function to remove from the head of the queue
+    def dequeue(self):
+        if self.head is None:
+            return None
+        
+        dequeue_node_value = self.head.value
+        self.head = self.head.next
+        self.num_nodes -= 1
+        return dequeue_node_value
+
+q = Queue()
+q.enqueue('a')
+q.enqueue('b')
+q.enqueue('c')
+
+print("Pass" if (q.size() == 3) else "Fail")
+q.enqueue(4)
+print("Pass" if (q.size() == 4) else "Fail")
+
+print("Pass" if (q.dequeue() == 'a') else "Fail")
+print("Pass" if (q.dequeue() == 'd') else "Fail")
+print("Pass" if (q.size() == 2) else "Fail")
