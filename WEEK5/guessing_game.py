d4ce66b26baf8a84864d86b8789ee06a65529a83
Oluchukwu Nwade <oluchinwade@gmail.com>
2023-10-30 21:46:39 +0000

    Binary sorting

--- WEEK5/guessing_game.py
+++ WEEK5/guessing_game.py
@@ -25,11 +25,12 @@ guess_random_number(5,0,10)'''
             
 """def guess_random_num_linear(tries, start, stop):
     random_number = random.randint(start, stop)
+    print(f"Random number to find is {random_number}")
     for x in range(start,stop+1):
         guess = x
 
         if guess == random_number:
-            print(f'You got it! the guessed number was {random_number}')
+            print(f'You got it! the guessed number is :{random_number}')
         
         elif guess < random_number:
             print("Guess Higher!")
@@ -45,6 +46,35 @@ guess_random_number(5,0,10)'''
             break
 
 guess_random_num_linear(5,0,10)"""
+
+def guess_random_num_binary(tries, start, stop):
+    ran_number = random.randint(start, stop)
+    print(f"Random number to find is: {ran_number}")
+    lower_bound = start
+    upper_bound = stop
+    while lower_bound <= upper_bound:
+        pivot = (lower_bound+upper_bound)//2
+
+        if pivot == ran_number:
+            print(f"Found it {ran_number}")
+            return 
+        elif pivot > ran_number:
+            upper_bound = pivot -1
+            tries -=1
+            print("Guess Lower")
+
+        else:
+            lower_bound = pivot +1
+            tries -= 1
+            print("Guess Higher!")
+        
+
+        if tries == 0:
+            print("Your program failed to find the number.")
+            return
+    
+
+guess_random_num_binary(5,0,100)
 
 
 
